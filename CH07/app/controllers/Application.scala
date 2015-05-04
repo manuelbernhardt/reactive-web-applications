37163ebe082d9bba246135eade9d21aa6fbbd862
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2015-05-04 13:29:39 +0200

    First jOOQ query

--- CH07/app/controllers/Application.scala
+++ CH07/app/controllers/Application.scala
@@ -3,7 +3,22 @@ package controllers
 import play.api._
 import play.api.Play.current
 import play.api.mvc._
+import play.api.db._
+
+import org.jooq._
+import org.jooq.impl.DSL
+import generated.Tables._
+import generated.tables.records._
+
 
 object Application extends Controller {
+
+  def authenticate = Action { implicit request =>
+    DB.withConnection { c =>
+      val context = DSL.using(c, SQLDialect.MYSQL)
+      val users = context.selectFrom[UserRecord](USER).fetch()
+      Ok(users.toString)
+    }
+  }
 
 }
