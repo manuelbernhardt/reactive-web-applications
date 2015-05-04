ca454f97e7e6c824e1f26ddc78e7ae5e6438a02b
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2015-05-04 15:03:46 +0200

    Login form and real authentication query

--- CH07/app/controllers/Application.scala
+++ CH07/app/controllers/Application.scala
@@ -3,8 +3,10 @@ package controllers
 import play.api._
 import play.api.Play.current
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 import play.api.db._
-
+import org.jooq.util.mysql.MySQLDSL._
 import org.jooq._
 import org.jooq.impl.DSL
 import generated.Tables._
@@ -13,12 +15,39 @@ import generated.tables.records._
 
 object Application extends Controller {
 
-  def authenticate = Action { implicit request =>
-    DB.withConnection { c =>
-      val context = DSL.using(c, SQLDialect.MYSQL)
-      val users = context.selectFrom[UserRecord](USER).fetch()
-      Ok(users.toString)
-    }
+  def login = Action { implicit request =>
+    Ok(views.html.login(loginForm))
   }
+
+  def authenticate = Action { implicit request =>
+    loginForm.bindFromRequest.fold(
+      formWithErrors =>
+        BadRequest(views.html.login(formWithErrors)),
+      login =>
+        DB.withConnection { c =>
+          val context = DSL.using(c, SQLDialect.MYSQL)
+          val users = context
+            .selectFrom[UserRecord](USER)
+            .where(USER.EMAIL.equal(login._1))
+            .and(USER.PASSWORD.equal(password(login._2)))
+            .fetch()
+
+          if (users.size() > 0) {
+            Ok(s"Hello ${users.get(0).getFirstname}")
+          } else {
+            BadRequest(
+              views.html.login(loginForm.withGlobalError("Wrong username or password"))
+            )
+          }
+        }
+    )
+  }
+
+  val loginForm = Form(
+    tuple(
+      "email" -> email,
+      "password" -> text
+    )
+  )
 
 }
