da00c52a9b11da7d6c03c325d05e2d8b6899e498
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2015-05-05 08:52:23 +0200

    Keeping authenticated users in the session

--- CH07/app/controllers/Application.scala
+++ CH07/app/controllers/Application.scala
@@ -7,13 +7,19 @@ import play.api.data._
 import play.api.data.Forms._
 import play.api.db._
 import org.jooq.util.mysql.MySQLDSL._
-import org.jooq._
+import org.jooq.SQLDialect
 import org.jooq.impl.DSL
 import generated.Tables._
 import generated.tables.records._
 
+import scala.concurrent.Future
+
 
 object Application extends Controller {
+
+  def index = Authenticated { request =>
+    Ok(views.html.index(request.firstName))
+  }
 
   def login = Action { implicit request =>
     Ok(views.html.login(loginForm))
@@ -33,7 +39,11 @@ object Application extends Controller {
             .fetch()
 
           if (users.size() > 0) {
-            Ok(s"Hello ${users.get(0).getFirstname}")
+            Redirect(routes.Application.index()).withSession(
+              Authenticated.USER_ID -> users.get(0).getId.toString,
+              Authenticated.FIRST_NAME -> users.get(0).getFirstname,
+              Authenticated.LAST_NAME -> users.get(0).getLastname
+            )
           } else {
             BadRequest(
               views.html.login(loginForm.withGlobalError("Wrong username or password"))
@@ -50,4 +60,31 @@ object Application extends Controller {
     )
   )
 
+}
+
+case class AuthenticatedRequest[A](userId: Long, firstName: String, lastName: String)
+
+object Authenticated extends ActionBuilder[AuthenticatedRequest] with Results {
+
+  val USER_ID = "userid"
+  val FIRST_NAME = "firstname"
+  val LAST_NAME = "lastname"
+
+  override def invokeBlock[A](request: Request[A], block: (AuthenticatedRequest[A]) => Future[Result]): Future[Result] = {
+    val authenticated = for {
+      id <- request.session.get(USER_ID)
+      firstName <- request.session.get(FIRST_NAME)
+      lastName <- request.session.get(LAST_NAME)
+    } yield {
+      AuthenticatedRequest[A](id.toLong, firstName, lastName)
+    }
+
+    authenticated.map { authenticatedRequest =>
+      block(authenticatedRequest)
+    } getOrElse {
+      Future.successful {
+        Redirect(routes.Application.login()).withNewSession
+      }
+    }
+  }
 }
