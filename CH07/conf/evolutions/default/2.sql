4985a7021afd4c4b0c98e976e254644c914e0124
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2015-05-19 08:16:45 +0200

    Evolution for the read model

---
+++ CH07/conf/evolutions/default/2.sql
@@ -0,0 +1,29 @@
+# --- !Ups
+
+CREATE TABLE "twitter_user" (
+  id bigserial primary key,
+  created_on timestamp with time zone NOT NULL,
+  phone_number varchar NOT NULL,
+  twitter_user_name varchar NOT NULL
+);
+
+CREATE TABLE "mentions" (
+  id bigserial primary key,
+  tweet_id varchar NOT NULL,
+  user_id bigint NOT NULL,
+  created_on timestamp with time zone NOT NULL,
+  author_user_name varchar NOT NULL,
+  text varchar NOT NULL
+);
+
+CREATE TABLE "mention_subscriptions" (
+  id bigserial primary key,
+  created_on timestamp with time zone NOT NULL,
+  user_id bigint NOT NULL
+)
+
+# --- !Downs
+
+DROP TABLE "twitter_user";
+DROP TABLE "mentions";
+DROP TABLE "mention_subscriptions";
