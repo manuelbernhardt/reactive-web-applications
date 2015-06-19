49a988ffa568823b2184a913c9947d1a2ebfe017
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2015-06-19 11:50:17 +0200

    Providing graph data from the server-side
    - WebSocket for retrieving / pushing the data
    - Querying the database from Chapter 7 to get the data (the query will be much shorter once jOOQ has support for generate_series with Timestamp)

--- CH08/app/controllers/Application.scala
+++ CH08/app/controllers/Application.scala
@@ -1,7 +1,19 @@
 package controllers
 
-import play.api.mvc._
+import java.sql.Timestamp
 
+import akka.actor.{Actor, ActorRef, Props}
+import dashboard.GraphType
+import generated.Tables._
+import org.joda.time.format.DateTimeFormat
+import org.joda.time.LocalDate
+import org.jooq.impl.DSL._
+import org.jooq.types.YearToMonth
+import org.jooq.{Converter, DatePart, SQLDialect}
+import play.api.Play.current
+import play.api.db._
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc._
 
 object Application extends Controller {
 
@@ -9,4 +21,73 @@ object Application extends Controller {
     Ok(views.html.index())
   }
 
+  def graphs = WebSocket.acceptWithActor[String, JsValue] {
+    request => out => DashboardClient.props(out)
+  }
+
+}
+
+class DashboardClient(out: ActorRef) extends Actor {
+  def graphType(s: String) = GraphType.withName(s)
+  def receive = {
+    case t: String => graphType(t) match {
+      case GraphType.MonthlySubscriptions =>
+        val mentionsCount = DB.withConnection { connection =>
+          val sql = using(connection, SQLDialect.POSTGRES_9_4)
+
+
+          // TODO do everything in the query once jOOQ supports timestamps in generate_series
+          /*
+            SELECT *
+            FROM (SELECT generate_series(now() - '1 month'::interval, now(), '1 day'::interval)::date) AS d(day)
+            LEFT JOIN (
+               SELECT date_trunc('day', created_on)::date AS day, count(*) AS mention_count
+               FROM   mentions
+               WHERE  created_on > now() - interval '1 month'
+               GROUP  BY 1
+               ) t USING (day)
+            ORDER  BY 1;
+          */
+
+          sql.select(trunc(MENTIONS.CREATED_ON, DatePart.DAY).as("day"), count())
+             .from(MENTIONS)
+             .where(MENTIONS.CREATED_ON.greaterThan(currentTimestamp().sub(new YearToMonth(0, 1))))
+             .groupBy(field("day"))
+             .orderBy(field("day"))
+             .fetch()
+        }
+
+        val allDates = (1 to 30).map { day =>
+          LocalDate.now.minusDays(day)
+        }
+
+        import scala.collection.JavaConverters._
+
+        val counts: Map[LocalDate, Int] = mentionsCount.iterator().asScala.map { record =>
+          record.getValue(0, new LocalDateConverter) -> record.getValue(1, classOf[Int])
+        }.toMap
+
+        val monthlyCounts: Map[String, Int] = allDates.map { day =>
+          DateTimeFormat.forPattern("dd/MM").print(day) -> counts.get(day).getOrElse(0)
+        }.sortBy(_._1).toMap
+
+
+        out ! Json.obj(
+          "graph_type" -> GraphType.MonthlySubscriptions,
+          "labels" -> Json.toJson(monthlyCounts.keys),
+          "series" -> Json.arr("Subscriptions"),
+          "data" -> Json.arr(Json.toJson(monthlyCounts.values))
+        )
+    }
+  }
+}
+object DashboardClient {
+  def props(out: ActorRef) = Props(classOf[DashboardClient], out)
+}
+
+class LocalDateConverter extends Converter[Timestamp, LocalDate] {
+  override def from(t: Timestamp): LocalDate = new LocalDate(t)
+  override def to(u: LocalDate): Timestamp = new Timestamp(u.toDateTimeAtStartOfDay.getMillis)
+  override def fromType(): Class[Timestamp] = classOf[Timestamp]
+  override def toType: Class[LocalDate] = classOf[LocalDate]
 }
--- CH08/build.sbt
+++ CH08/build.sbt
@@ -1,11 +1,16 @@
 lazy val scalaV = "2.11.6"
 
-lazy val root = (project in file(".")).settings(
+lazy val `ch08` = (project in file(".")).settings(
   scalaVersion := scalaV,
   scalaJSProjects := Seq(client),
   pipelineStages := Seq(scalaJSProd),
   libraryDependencies ++= Seq(
-    "com.vmunier" %% "play-scalajs-scripts" % "0.2.2"
+    "com.vmunier" %% "play-scalajs-scripts" % "0.2.2",
+    "org.postgresql" % "postgresql" % "9.4-1201-jdbc41",
+    jdbc,
+    "org.jooq" % "jooq" % "3.6.0",
+    "org.jooq" % "jooq-codegen-maven" % "3.6.0",
+    "org.jooq" % "jooq-meta" % "3.6.0"
   ),
   WebKeys.importDirectly := true
 ).enablePlugins(PlayScala).dependsOn(client).aggregate(client)
@@ -19,10 +24,20 @@ lazy val client = (project in file("modules/client")).settings(
     "biz.enef"     %%% "scalajs-angulate" % "0.2"
   ),
   jsDependencies ++= Seq(
-    "org.webjars" % "angularjs" % "1.3.15" / "angular.min.js",
-    "org.webjars" % "angularjs" % "1.3.15" / "angular-route.min.js" dependsOn "angular.min.js",
-    "org.webjars.bower" % "angular-websocket" % "1.0.13" / "angular-websocket.min.js"
+    "org.webjars.bower" % "angular" % "1.4.0" / "angular.min.js",
+    "org.webjars.bower" % "angular-route" % "1.4.0" / "angular-route.min.js" dependsOn "angular.min.js",
+    "org.webjars.bower" % "angular-websocket" % "1.0.13" / "dist/angular-websocket.min.js" dependsOn "angular.min.js"
   ),
   skip in packageJSDependencies := false
-
 ).enablePlugins(ScalaJSPlugin, ScalaJSPlay, SbtWeb)
+
+val generateJOOQ = taskKey[Seq[File]]("Generate JooQ classes")
+
+val generateJOOQTask = (sourceManaged, dependencyClasspath in Compile, runner in Compile, streams) map { (src, cp, r, s) =>
+  toError(r.run("org.jooq.util.GenerationTool", cp.files, Array("conf/chapter7.xml"), s.log))
+  ((src / "main/generated") ** "*.scala").get
+}
+
+generateJOOQ <<= generateJOOQTask
+
+sourceGenerators in Compile <+= generateJOOQTask
