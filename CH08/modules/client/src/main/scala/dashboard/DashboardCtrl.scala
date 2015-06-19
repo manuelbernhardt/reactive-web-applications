70e918b453421ac5f9ce166f1d66fecca76002d3
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2015-06-19 11:51:19 +0200

    Using the GraphDataService from the DashboardCtrl

--- CH08/modules/client/src/main/scala/dashboard/DashboardCtrl.scala
+++ CH08/modules/client/src/main/scala/dashboard/DashboardCtrl.scala
@@ -2,9 +2,12 @@ package dashboard
 
 import biz.enef.angulate._
 import org.scalajs.dom._
+import scala.scalajs.js
 import scalajs.js.Dynamic
 
-class DashboardCtrl($scope: Dynamic) extends ScopeController {
-  $scope.hello = "Hello, world"
-  $scope.helloBack = () => console.log("Hi")
+class DashboardCtrl($scope: Dynamic, graphDataService: GraphDataService) extends ScopeController {
+  graphDataService.fetchGraph(GraphType.MonthlySubscriptions, { (graphData: js.Dynamic) =>
+    console.log(graphData)
+    $scope.monthlySubscriptions = graphData
+  })
 }
